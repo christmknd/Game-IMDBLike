49ecf919a333b7b737f61e082c7c83f7937eabc7
Christ MTAE <christ@mtae.local>
2023-09-11 16:44:37 +0200

    API is READY to be used

--- gil-front/components/game/GameSearch.vue
+++ gil-front/components/game/GameSearch.vue
@@ -1,38 +1,57 @@
 <script setup>
+import { ref } from 'vue';
 
-const config = useRuntimeConfig()
-const apiKey = config.public.RAWG_API_KEY;
-console.log(apiKey)
+const config = useRuntimeConfig();
+const apiKey = config.public.RAWG_API_KEY
 
-const searchGame = async () => {
-  try {
-    
-  } catch (error) {
-    
+
+
+const searchQuery = ref('');
+const games = ref([]);
+const loading = ref(false);
+
+const searchGames = async () => {
+  if (!searchQuery.value) {
+    return;
   }
-}
+
+  loading.value = true;
+
+  try {
+    const response = await $fetch(
+      `https://api.rawg.io/api/games?page_size=10&search=${searchQuery.value}&key=${apiKey}`
+    );
+    games.value = response.results;
+  } catch (error) {
+    console.error('Erreur lors de la recherche de jeux : ', error);
+  } finally {
+    loading.value = false;
+  }
+};
 </script>
 
 <template>
-  <div class="gameSearch">
-    <h1>Recherchez des jeux</h1>
-    <form @submit.prevent="searchGame()">
-    <input placeholder="Rechercher un livre" />
-    <input type="button" value="Rechercher">
+  <div>
+    <form @submit.prevent="searchGames">
+      <input v-model="searchQuery" placeholder="Rechercher un jeu" />
+      <button type="submit">Rechercher</button>
     </form>
-    <div class="result">
-      <p>{{  }}</p>
+
+    <div v-if="loading">Chargement en cours...</div>
+
+    <div v-else>
+      <ul>
+        <li v-for="game in games" :key="game.id">{{ game.name }}</li>
+      </ul>
     </div>
   </div>
 </template>
 
 <style scoped>
 .gameSearch {
-  div {
   display: flex;
   justify-content: center;
   align-items: center;
   flex-direction: column;
-}
 }
 </style>
--- gil-front/nuxt.config.ts
+++ gil-front/nuxt.config.ts
@@ -1,4 +1,4 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config<
+// https://nuxt.com/docs/api/configuration/nuxt-config
 import {resolve} from 'path'
 
 

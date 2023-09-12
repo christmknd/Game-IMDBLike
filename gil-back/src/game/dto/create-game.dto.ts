7fa41f5f8e8294cbd83405f98a7457249ce415ee
Christ MTAE <christ@mtae.local>
2023-09-12 10:55:56 +0200

    Add game entity & DTO

--- gil-back/src/game/dto/create-game.dto.ts
+++ gil-back/src/game/dto/create-game.dto.ts
@@ -1,1 +1,18 @@
-export class CreateGameDto {}
+import { ApiProperty } from '@nestjs/swagger';
+import { IsNotEmpty } from 'class-validator';
+
+export class CreateGameDto {
+  @ApiProperty({ description: 'Game Title' })
+  @IsNotEmpty()
+  name: string;
+
+  @ApiProperty({ description: 'Released Date of the game' })
+  @IsNotEmpty()
+  releasedDate: string;
+
+  @ApiProperty({ description: 'Genre(s) of the game' })
+  genres: string[];
+
+  @ApiProperty({ description: 'Platform(s) you can play the game on' })
+  platforms: string[];
+}

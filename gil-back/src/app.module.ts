fb4811add9388c16552721bd11a1508a5e63f137
christmknd <christmokonda@gmail.com>
2023-09-13 22:34:07 +0200

    nest g resource bookmark

--- gil-back/src/app.module.ts
+++ gil-back/src/app.module.ts
@@ -6,23 +6,26 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Game } from './game/entities/game.entity';
 import { ShopModule } from './shop/shop.module';
 import { Shop } from './shop/entities/shop.entity';
+import { BookmarkModule } from './bookmark/bookmark.module';
+import { Bookmark } from './bookmark/entities/bookmark.entity';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot({
       type: 'mysql',
       host: 'localhost',
-      port: 3306,
-      username: 'root',
-      password: 'mtae123',
+      port: 3306, //8889
+      username: 'root', 
+      password: 'mtae123', //root
       database: 'gameapp',
-      entities: [Game, Shop],
+      entities: [Game, Shop, Bookmark],
       logging: true,
       synchronize: false,
       migrations: ['dist/migrations/*js'],
     }),
     GameModule,
     ShopModule,
+    BookmarkModule,
   ],
   controllers: [AppController],
   providers: [AppService],

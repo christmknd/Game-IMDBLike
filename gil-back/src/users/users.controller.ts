c320af8061132f8fb41e6758dec3d5415ad9d8b9
Christ MTAE <christ@mtae.local>
2023-09-14 09:42:20 +0200

    ADD user service & controller

--- gil-back/src/users/users.controller.ts
+++ gil-back/src/users/users.controller.ts
@@ -1,34 +1,102 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  BadRequestException,
+  NotFoundException
+} from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import {
+  ApiBadRequestResponse,
+  ApiBody,
+  ApiNotFoundResponse,
+  ApiOperation,
+  ApiParam,
+  ApiResponse,
+  ApiTags,
+} from '@nestjs/swagger';
+import { User } from './entities/user.entity';
 
+@ApiTags('users')
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
+  @ApiOperation({ summary: 'Create a new user' })
+  @ApiBody({ type: CreateUserDto })
+  @ApiResponse({
+    status: 201,
+    description: 'User created successfully',
+    type: User,
+  })
+  @ApiBadRequestResponse({ description: 'User cannot be registrated' })
   @Post()
   create(@Body() createUserDto: CreateUserDto) {
-    return this.usersService.create(createUserDto);
+    try {
+      return this.usersService.createUser(createUserDto);
+    } catch {
+      throw new BadRequestException('User cannot be registrated');
+    }
   }
 
+  @ApiOperation({ summary: 'Get all users' })
+  @ApiNotFoundResponse({ description: 'No users found' })
   @Get()
   findAll() {
-    return this.usersService.findAll();
+    try {
+      return this.usersService.findAllUsers();
+    } catch {
+      throw new NotFoundException('No users found');
+    }
   }
 
+  @ApiOperation({ summary: 'Get user by ID' })
+  @ApiParam({ name: 'id', type: Number })
+  @ApiResponse({ status: 200, description: 'Return user by ID', type: User })
+  @ApiNotFoundResponse({ description: 'User not found' })
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.usersService.findOne(+id);
+  findUserById(@Param('id') id: string) {
+    try {
+      return this.usersService.findUserById(+id);
+    } catch {
+      throw new NotFoundException('User not found');
+    }
   }
 
+  @ApiOperation({ summary: 'Update user by ID' })
+  @ApiParam({ name: 'id', type: Number })
+  @ApiBody({ type: UpdateUserDto })
+  @ApiResponse({
+    status: 200,
+    description: 'User updated successfully',
+    type: User,
+  })
+  @ApiNotFoundResponse({ description: 'User not found : cannot be updated' })
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
-    return this.usersService.update(+id, updateUserDto);
+    try {
+      return this.usersService.updateUser(+id, updateUserDto);
+    } catch {
+      throw new NotFoundException('User not found : cannot be updated');
+    }
   }
 
+  @ApiOperation({ summary: 'Delete user by ID' })
+  @ApiParam({ name: 'id', type: Number })
+  @ApiResponse({ status: 200, description: 'User deleted successfully' })
+  @ApiNotFoundResponse({ description: 'User not found : cannot be deleted' })
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.usersService.remove(+id);
+  delete(@Param('id') id: string) {
+    try {
+      return this.usersService.deleteUser(+id);
+    } catch {
+      throw new NotFoundException('User not found : cannot be deleted');
+    }
   }
 }
--- gil-back/src/users/users.service.ts
+++ gil-back/src/users/users.service.ts
@@ -1,26 +1,44 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from "@nestjs/common";
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { User } from './entities/user.entity';
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
 
 @Injectable()
 export class UsersService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+
+  constructor(
+    @InjectRepository(User)
+    private userRepository: Repository<User>,
+  ) {}
+  async createUser(createUserDto: CreateUserDto): Promise<User> {
+    const user = this.userRepository.create(createUserDto);
+    return this.userRepository.save(user);
   }
 
-  findAll() {
-    return `This action returns all users`;
+  async findAllUsers(): Promise<User[]> {
+    return this.userRepository.find();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
+  async findUserById(id: number): Promise<User> {
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
+    const user = await this.userRepository.findOneBy({ id: id });
+    if (!user) {
+      throw new NotFoundException(`User with id ${id} not found`);
+    }
+    return user;
   }
 
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
+  async updateUser(id: number, updateUserDto: UpdateUserDto): Promise<User> {
+    const user = await this.userRepository.findOneBy({ id: id });
+    Object.assign(user, updateUserDto);
+    return this.userRepository.save(user);
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} user`;
+  async deleteUser(id: number): Promise<void> {
+    const user = await this.userRepository.findOneBy({ id: id });
+    await this.userRepository.remove(user);
   }
 }

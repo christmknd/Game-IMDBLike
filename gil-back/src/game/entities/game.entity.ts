1410e1d6b707640aa91dc3fdb513b7c76cb286f8
Christ MTAE <christ@mtae.local>
2023-09-14 12:51:22 +0200

    Add entities relationships

--- gil-back/src/game/entities/game.entity.ts
+++ gil-back/src/game/entities/game.entity.ts
@@ -1,7 +1,15 @@
 // game.entity.ts
 
-import { Entity, Column, PrimaryGeneratedColumn, OneToMany } from 'typeorm';
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  OneToMany,
+  ManyToOne,
+} from 'typeorm';
 import { Bookmark } from '../../bookmark/entities/bookmark.entity';
+import { Review } from '../../review/entities/review.entity';
+import { User } from '../../users/entities/user.entity';
 
 @Entity({ name: 'game' })
 export class Game {
@@ -20,6 +28,15 @@ export class Game {
   @Column('json')
   platforms: string;
 
-  @OneToMany(() => Bookmark, (bookmark) => bookmark.game)
+  //Les jeux sont ajoutés par les utilisateurs.
+  @ManyToOne(() => User, (user) => user.games)
+  user: User;
+
+  //Un jeu peut avoir plusieurs reviews (de différents utilisateurs).
+  @OneToMany(() => Review, (review) => review.games)
+  reviews: Review[];
+
+  //Un jeu peut être bookmarké par plusieurs utilisateurs (être dans leurs favoris).
+  @OneToMany(() => Bookmark, (bookmark) => bookmark.games)
   bookmarks: Bookmark[];
 }

fa1834a594055d9ced44d3a891446c0d1f27db18
Christ MTAE <christ@mtae.local>
2023-09-18 18:10:29 +0200

    Code block

---
+++ gil-front/pages/game/addgame.vue
@@ -0,0 +1,16 @@
+<script  setup>
+definePageMeta({
+  layout: "user",
+});
+useHead({
+  title: 'Ajouter un jeu'
+})
+</script>
+
+<template>
+  <div>
+    <GameForm/>
+  </div>
+</template>
+
+<style scoped></style>

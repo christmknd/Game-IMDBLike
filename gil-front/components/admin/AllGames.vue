f00e88a4fb374737c767bcdaf27c70d026bd8951
Christ MTAE <christ@mtae.local>
2023-09-19 12:00:03 +0200

    Backend requests works well

--- gil-front/components/admin/AllGames.vue
+++ gil-front/components/admin/AllGames.vue
@@ -1,44 +1,20 @@
 <template>
   <div>
     <h1>Liste des jeux</h1>
-      <li v-for="game in games" :key="game.id">
+      <div v-for="game in games" :key="game.id">
         <ul>
           <li><h2>{{ game.name }}</h2></li>
           <li><em>{{ game.releaseYear }}</em></li>
           <li>{{ game.genre }}</li>
           <li>{{ game.platform }}</li>
         </ul>
-      </li>
+      </div>
     </div>
 </template>
 
 
-<script>
-export default {
-
-  data () {
-    return {
-      games : [],
-    }
-  },
-  method: {
-   async getAllgames () {
-    try {
-    const response = await $fetch('http://localhost:5000/game', {
-      method : 'GET',
-      headers: {
-          'Content-Type' : 'application/json',
-      }
-    })
-    const data = await response;
-    this.games.push(...data)
-    return this.games
-  } catch (error) {
-    console.error('Erreur lors de la recupérations des jeux', error)
-  }
-   },
-  },
-}
-
-
+<script setup>
+  const { data: games } = await useFetch(
+      `http://localhost:5000/game`
+    );
 </script>

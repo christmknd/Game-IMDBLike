4bc5511967193cd42b6d62aa80480a7abff7f8b2
Christ MTAE <christ@mtae.local>
2023-10-06 16:36:59 +0200

    Code block vuelidate

--- gil-front/components/auth/LoginForm.vue
+++ gil-front/components/auth/LoginForm.vue
@@ -4,66 +4,61 @@
     <form @submit.prevent="login">
       <div class="mb-3">
         <label htmlFor='username' class="form-label">Username</label>
-        <input 
+        <input
           class="form-control"
-          type="text" 
-          id="username" 
+          type="text"
+          id="username"
           placeholder='username'
-          v-model="form_username" 
+          v-model="form_username"
           />
       </div>
       <div class="mb-3">
         <label htmlFor='password' class="form-label">Mot de passe</label>
-          <input 
+        <input
           class="form-control"
-          type="password" 
-          id="password" 
+          type="password"
+          id="password"
           placeholder="password"
           v-model="form_password"
           />
       </div>
-          <button type="submit" class="btn btn-primary">Se connecter</button>
-      </form>
+      <button type="submit" class="btn btn-primary">Se connecter</button>
+    </form>
   </div>
 </template>
 
-<script>
-import auth from '~/services/auth';
-import useValidate from '@vuelidate/core'
-import { required } from '@vuelidate/validators'
+<script setup>
+import { ref } from 'vue';
+import { useVuelidate } from '@vuelidate/core';
+import {required, minLength} from '@vuelidate/validators'
 
-export default {
-  data (){
-    return {
-        v$: useValidate(),
-        form_username : '',
-        form_password : ''
-    }
+const form_username = ref('');
+const form_password = ref('');
+
+const rules = {
+  form_username: {
+    required
   },
-  methods: {
-    async login () {
-      try {
-       await auth.login(this.form_username, this.form_password);
-        this.$emit('user-logged');
-        this.v$.$validate()
-        if (!this.v$.$errors){
-          alert('Les informations que vous venez d\'entrer sont incorrects')
-        } else {
-          this.$router.push('/game');
-        console.log('User connecté sur la plateforme avec succès')
-        }
-      } catch (error) {
-        console.error('Une erreur s\'est produite lors de la connexion du user : ',error )
-      }
-    },
-    validations () {
-      return {
-        form_username: {required},
-        form_password: {required}
-      }
-    },
-  }
-}
-</script>
+  form_password: {
+    required,
+    minLength: minLength(8),
+  },
+};
 
-<style scoped></style>
+const v$ = useVuelidate(rules);
+
+
+const login = async () => {
+  v$.validate();
+
+  if (!v.$error) {
+    try {
+      await auth.login(form_username.value, form_password.value);
+      emit('user-logged');
+      router.push('/game');
+    } catch (error) {
+      console.error('Erreur de connexion : ', error);
+    }
+  }
+};
+</script>

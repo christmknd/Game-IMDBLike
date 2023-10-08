fc6bd6737fa1af2f28e037ec7e50f18594914244
christmknd <christmokonda@gmail.com>
2023-10-09 01:25:52 +0200

    Block path undefinded

--- gil-front/middleware/connected.js
+++ gil-front/middleware/connected.js
@@ -1,9 +1,18 @@
-import authService from '~/services/auth'; 
+import authService from '~/services/auth';
 
-export default defineNuxtRouteMiddleware((to, from) => {
+export default defineNuxtRouteMiddleware( ({ to, from }) => {
   const isAuthenticated = authService.isConnected();
-  
+  const userRole = authService.getRole();
+
+  const anonymousPages = ['/login', '/register', '/'];
+
   if (!isAuthenticated) {
-    return navigateTo('/login')
+    if (!anonymousPages.includes(to.path)) {
+      return navigateTo('/login');
+    }
+  } else if (isAuthenticated) {
+    if (userRole === 'Player' && to.path == '/admin') {
+        return navigateTo('/game');
   }
+}
 });
